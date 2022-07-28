1dc12e581213d883ed0946fc6b87322e78e67822
rakshithkalmadi <rakshithkalmadi@gmail.com>
2022-07-28 22:51:32 +0530

    social profilea

--- src/data/socialsData.js
+++ src/data/socialsData.js
@@ -1,14 +1,14 @@
 export const socialsData = {
-    github: 'https://github.com/',
-    facebook: 'https://www.facebook.com/',
-    linkedIn: 'https://www.linkedin.com/in',
-    instagram: 'https://www.instagram.com/',
-    codepen: 'https://codepen.io/',
-    twitter: 'https://twitter.com/',
-    reddit: 'https://www.reddit.com/user/',
-    blogger: 'https://www.blogger.com/',
-    medium: 'https://medium.com/@',
-    stackOverflow: 'https://stackoverflow.com/users/',
-    gitlab: 'https://gitlab.com/',
-    youtube: 'https://youtube.com/'
+    github: 'https://github.com/rakshithkalmadi',
+    // facebook: 'https://www.facebook.com/',
+    linkedIn: 'https://www.linkedin.com/in/rakshithkalmadi/',
+    instagram: 'https://www.instagram.com/rakshithkalmadi',
+    // codepen: 'https://codepen.io/',
+    twitter: 'https://twitter.com/rakshithkalmadi',
+    // reddit: 'https://www.reddit.com/user/',
+    // blogger: 'https://www.blogger.com/',
+    // medium: 'https://medium.com/@',
+    // stackOverflow: 'https://stackoverflow.com/users/',
+    // gitlab: 'https://gitlab.com/',
+    youtube: 'https://www.youtube.com/channel/UCKx-2zSHoARb5PRV3JJxbZQ/'
 }
