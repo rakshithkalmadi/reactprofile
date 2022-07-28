db0e40433aedcf59d23c993f3a5dcf4de4f575a2
rakshithkalmadi <rakshithkalmadi@gmail.com>
2022-07-28 22:37:32 +0530

    changes

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -2,7 +2,7 @@ import resume from '../assets/pdf/resume.pdf'
 
 export const headerData = {
     name: 'Rakshith H Kalmadi',
-    title: "Web Developer",
+    title: "Software Engineer",
     desciption:"Aspiring computer programmer with a BTech degree and proven problem-solving and troubleshooting skills. Seeking an entry-level programmer or coder position to continue expanding my knowledge of different languages and systems.",
     image: 'https://raw.githubusercontent.com/PhantomScript/asset-container/main/developer-portfolio/landingImg.png',
     resumePdf: resume
